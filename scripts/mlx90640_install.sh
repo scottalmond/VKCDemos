fa7a5aa7a74100cffadda9b8f282912b8393ba7f
paarthurnax <turret>
2019-08-13 09:44:39 -0700

    updated mpl90640 whl and installer

--- scripts/mlx90640_install.sh
+++ scripts/mlx90640_install.sh
@@ -13,20 +13,22 @@ $rcn do_i2c 0
 
 if [[ $do_installs == true ]]; then
   echo Lets install some files
-  $apti read-edid i2c-tools wiringpi git vim
-  $apti libgirepository-1.0 gir1.2-glib
-  $apti libavutil-dev libavcodec-dev libavformat-dev libsdl2-dev
-  $apti libpython3-dev python3-pip python3-smbus python3-venv python3-gi python3-gi-cairo gir1.2-gtk-3.0
-  $apti liblapack3 libcblas3 libatlas-dev libatlas3-base libjasper1 openexr libswscale4 libqtgui4 libqt4-test
-  $apti python-gi python3-gi \
+  pkgs=(read-edid i2c-tools wiringpi git vim)
+  pkgs=(libgirepository-1.0 gir1.2-glib)
+  pkgs=(libavutil-dev libavcodec-dev libavformat-dev libsdl2-dev)
+  pkgs=(libpython3-dev python3-pip python3-smbus python3-venv python3-gi python3-gi-cairo gir1.2-gtk-3.0)
+  pkgs=(liblapack3 libatlas-base-dev libatlas3-base libjasper1 openexr libswscale4 libqtgui4 libqt4-test)
+  pkgs=(python-gi python3-gi \
     gstreamer1.0-tools \
     gir1.2-gstreamer-1.0 \
     gir1.2-gst-plugins-base-1.0 \
-    gstreamer1.0-libav
-  $apti gstreamer1.0-tools gstreamer1.0-omx-rpi gstreamer1.0-plugins-{base,good,ugly,bad}
-  $apti gstreamer1.0-omx-rpi-config gstreamer1.0-omx
+    gstreamer1.0-libav)
+      pkgs=(gstreamer1.0-tools gstreamer1.0-omx-rpi gstreamer1.0-plugins-{base,good,ugly,bad})
+  pkgs=(gstreamer1.0-omx-rpi-config gstreamer1.0-omx)
 
-  $apti libi2c-dev swig3.0 tcl8.6-dev
+  pkgs=(libi2c-dev swig3.0 tcl8.6-dev)
+
+  $apti ${pkgs[@]}
 fi
 
 usermod -a -G i2c pi
@@ -34,25 +36,26 @@ if ! grep -q "dtparam=i2c1_baudrate=1000000"   /boot/config.txt ; then
     echo      dtparam=i2c1_baudrate=1000000 >> /boot/config.txt
 fi
 
-pushd ${VKCDemos_src}/libs/mlx90640-library
-sed 's/bcm2835-1.55/bcm2835-1.59/g' Makefile -i
-make bcm2835
-make I2C_MODE=LINUX libMLX90640_API.so
-popd
+#pushd ${VKCDemos_src}/libs/mlx90640-library
+#sed 's/bcm2835-1.55/bcm2835-1.60/g' Makefile -i
+#make bcm2835
+#make I2C_MODE=LINUX libMLX90640_API.so
+#popd
 
 function setup_vkc_python_env() {
 if [ ! -d ${pyvenv_dir} ] ; then
 	echo creating environment
-    pyvenv-3.5 --system-site-packages ${pyvenv_dir}
+    python3 -m venv --system-site-packages ${pyvenv_dir}
 fi
 
 . ${pyvenv_dir}/bin/activate
 
 python -m pip install --user pip
-python -m pip install opencv-python Pillow
+#python -m pip install opencv-python
+python -m pip install Pillow
 
 if [[ ${use_whl} == true ]] ; then
-  python -m pip install ${VKCDemos_src}/scripts/python-pkgs/MLX90640-0.0.2-cp35-none-linux_armv7l.whl
+  python -m pip install ${VKCDemos_src}/scripts/python-pkgs/MLX90640-0.0.2-cp37-cp37m-linux_armv7l.whl
 else
   pushd ${VKCDemos_src}/libs/mlx90640-library/python/library/
   make
--- scripts/rpi_cam_install.sh
+++ scripts/rpi_cam_install.sh
@@ -10,20 +10,24 @@ $rcn do_camera 0
 
 if [[ $do_installs == true ]]; then
   echo Lets install some files
-  $apti read-edid i2c-tools wiringpi git vim
-  $apti libgirepository-1.0 gir1.2-glib
-  $apti libavutil-dev libavcodec-dev libavformat-dev libsdl2-dev
-  $apti libpython3-dev python3-pip python3-smbus python3-venv python3-gi python3-gi-cairo gir1.2-gtk-3.0
-  $apti liblapack3 libcblas3 libatlas-dev libatlas3-base libjasper1 openexr libswscale4 libqtgui4 libqt4-test
-  $apti python-gi python3-gi \
+  pkgs=(git vim)
+  pkgs+=(read-edid i2c-tools wiringpi)
+  pkgs+=(libgirepository-1.0 gir1.2-glib)
+  pkgs+=(libavutil-dev libavcodec-dev libavformat-dev libsdl2-dev)
+  pkgs+=(libpython3-dev python3-pip python3-smbus python3-venv python3-gi python3-gi-cairo gir1.2-gtk-3.0)
+  pkgs+=(liblapack3 libatlas-base-dev libatlas3-base libjasper1 openexr libswscale{5,-dev} libqtgui4 libqt4-test)
+  pkgs+=(python-gi python3-gi \
     gstreamer1.0-tools \
     gir1.2-gstreamer-1.0 \
     gir1.2-gst-plugins-base-1.0 \
-    gstreamer1.0-libav
-  $apti gstreamer1.0-tools gstreamer1.0-omx-rpi gstreamer1.0-plugins-{base,good,ugly,bad}
-  $apti gstreamer1.0-omx-rpi-config gstreamer1.0-omx
+    gstreamer1.0-libav)
+  pkgs+=(gstreamer1.0-tools gstreamer1.0-omx-rpi gstreamer1.0-plugins-{base,good,ugly,bad})
+  pkgs+=(gstreamer1.0-omx-rpi-config gstreamer1.0-omx)
 
-  $apti uvcdynctrl
+  pkgs+=(uvcdynctrl)
+  pkgs+=(python3-opencv)
+
+  $apti ${pkgs[@]}
 fi
 
 usermod -a -G video pi
@@ -34,13 +38,14 @@ fi
 function setup_vkc_python_env() {
 if [ ! -d ${pyvenv_dir} ] ; then
 	echo creating environment
-    pyvenv-3.5 --system-site-packages ${pyvenv_dir}
+    python3 -m venv --system-site-packages ${pyvenv_dir}
 fi
 
 . ${pyvenv_dir}/bin/activate
 
 python -m pip install --user pip
-python -m pip install opencv-python Pillow "picamera[array]"
+#python -m pip install opencv-python
+python -m pip install Pillow "picamera[array]"
 
 deactivate
 
